ac243f83e9b3c6b17d9899937952fa7e43b25ef9
Xander <xanderhogan13@gmail.com>
2024-10-20 09:21:49 -0400

    Update remind.ts

--- src/commands/remind.ts
+++ src/commands/remind.ts
@@ -5,25 +5,66 @@ import {
 	type CommandInteraction,
 } from "@buape/carbon";
 
+let targetId = "";
+let userId = "";
+
+const xander = "829909201262084096";
+const strings = "343915513380077569";
+const wifey = "1070876750445293628";
+
 export default class RemindCommand extends Command {
-	name = "update-modpack";
-	description = "Remind people to update their modpack.";
+	name = "remind";
+	description = "Remind someone of something!";
 	defer = true;
 
 	options: APIApplicationCommandBasicOption[] = [
 		{
-			name: "who",
-			type: ApplicationCommandOptionType.String,
+			name: "user",
+			type: ApplicationCommandOptionType.User,
 			description: "Who would you like to ask to remind?",
+			required: true,
+		},
+		{
+			name: "about",
+			type: ApplicationCommandOptionType.String,
+			description: "What are you reminding them about?",
 			required: true,
 		},
 	];
 
 	async run(interaction: CommandInteraction) {
-		const who = interaction.options.getString("who", true);
+		if (!interaction.userId) return interaction.reply("You aren't a user");
 
-		await interaction.reply({
-			content: `Hey, dont forget to update your modpack!\n\n${who}`,
-		});
+		userId = interaction.userId?.toString();
+		const target = interaction.options.getUser("user", true);
+		targetId = target.id;
+
+		const about = interaction.options.getString("about", true);
+
+		// For Xander
+		if (userId === xander) {
+			if (targetId === strings) {
+				//  Strings
+				await interaction.reply({
+					content: `Hey Stringssssssss, dont forget: \n\n${about}`,
+				});
+			} else if (targetId === wifey) {
+				// Wifey
+				await interaction.reply({
+					content: `Hey princess, dont forget: \n\n${about}`,
+				});
+			} else {
+				// others
+				await interaction.reply({
+					content: `Hey bro, dont forget: \n\n${about}\n\n<@${targetId}>`,
+				});
+			}
+		}
+		// For anyone else
+		else {
+			await interaction.reply({
+				content: `Hey man, don't forget: \n\n${about}\n\n<@${targetId}>`,
+			});
+		}
 	}
 }
